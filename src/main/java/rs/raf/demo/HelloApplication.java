03ce5de4e7931097a72dd6079dcfb596480a799f
Antic <stefan.antic@casasoft.com>
2023-05-06 13:04:02 +0200

    initial commit

---
+++ src/main/java/rs/raf/demo/HelloApplication.java
@@ -0,0 +1,35 @@
+package rs.raf.demo;
+
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
+import rs.raf.demo.repositories.subject.InMemorySubjectRepository;
+import rs.raf.demo.repositories.subject.SubjectRepository;
+import rs.raf.demo.services.SubjectService;
+
+import javax.inject.Singleton;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/api")
+public class HelloApplication extends ResourceConfig {
+
+    public HelloApplication() {
+        // Ukljucujemo validaciju
+        property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
+
+        // Definisemo implementacije u dependency container-u
+        AbstractBinder binder = new AbstractBinder() {
+            @Override
+            protected void configure() {
+                this.bind(InMemorySubjectRepository.class).to(SubjectRepository.class).in(Singleton.class);
+
+                this.bindAsContract(SubjectService.class);
+
+            }
+        };
+        register(binder);
+
+        // Ucitavamo resurse
+        packages("rs.raf.demo.resources");
+    }
+}
